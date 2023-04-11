7bbca8dcd568a87bc1dc6d4765f815697ce93fe9
an-Arkhan <71709107+an-Arkhan@users.noreply.github.com>
2023-04-12 01:14:03 +0700

    changing viewport size of smartwatch

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,18 +4,17 @@ export const Dashboard = () => {
     return (
         <div className="Smartwatch m-auto mt-5">
             <div className="content-center">
-                <div className="grid grid-cols-5 mx-auto bottomMiddle">
-                    <div className="m-auto symbol">!@#</div>
-                    <div className="my-auto px-5 number">123</div>
-                    <div className="col-span-2 my-auto px-12 space">Space</div>
-                    <div className="m-auto backspace">⌫</div>
+                <div className="grid grid-cols-4 mx-auto bottomMiddle">
+                    <div className="my-auto px-2 symbol">!@#</div>
+                    <div className="my-auto px-1 number">123</div>
+                    <div className="my-auto px-1 space">space</div>
+                    <div className="my-auto px-2.5 backspace">⌫</div>
                 </div>
-                <div className="grid grid-cols-5 mx-auto bottomButton">
-                    <div className="m-auto back">&larr;</div>
-                    <div className="my-auto px-5 firstAlpha">ABC</div>
-                    <div className="m-auto secondAlpha">DEF</div>
-                    <div className="my-auto px-5 thirdAlpha">GHI</div>
-                    <div className="m-auto next">&rarr;</div>
+                <div className="grid grid-cols-4 mx-auto bottomButton">
+                    <div className="my-auto px-4 back">&larr;</div>
+                    <div className="my-auto px-1 firstAlpha">ABC</div>
+                    <div className="my-auto px-1 secondAlpha">DEF</div>
+                    <div className="my-auto px-2.5 next">&rarr;</div>
                 </div>
             </div>
         </div>
